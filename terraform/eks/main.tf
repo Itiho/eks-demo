dca4fd781ce6c38685b191cc42da6a4b455c7b6b
Willian <itihoitiho@gmail.com>
2021-09-27 14:41:08 -0400

    minimo

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -65,7 +65,7 @@ module "eks" {
   worker_groups_launch_template = [
     {
       name                    = "spots"
-      override_instance_types = ["t3.micro"]
+      override_instance_types = ["t3.medium"]
       spot_instance_pools     = 2
       asg_max_size            = 1
       asg_min_size            = 1
